d3fade02f99617db9ae3f72f99ea2d825ccbd84d
Max G <maxdotdotg@gmail.com>
2020-07-10 16:15:46 -0400

    deps and unit file

--- packer/setup.sh
+++ packer/setup.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 
 # update
-apt update -y && apt upgrade -y
+apt update -y 
 
 # install apt deps
-apt install -y python-dev \
-    python-pip \
-    git
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
+	build-essential \
+	python-dev \
+	python-pip \
+	libffi-dev
+    python-pip-whl \
+    git \
+	zip
 
 # install pip deps
 pip install gunicorn
 
-# clone the repo
-git clone https://github.com/isislab/CTFd.git /opt
+#get ctfd
+wget https://github.com/CTFd/CTFd/archive/2.5.0.zip
+unzip 2.5.0.zip -d /opt
 
+# create ctfd user
+sudo useradd ctfd -d /opt/2.5.0
+sudo chown -R ctfd.ctfd /opt/2.5.0
+
+# install python deps
+sudo su ctfd -c "pip install -r /opt/2.5.0/requirements.txt"
+
+# copy systemd unit
+cp /vagrant/ctfd.service /etc/systemd/system/ctfd.service
+
+# reload systemd
+sudo systemctl daemon-reload
