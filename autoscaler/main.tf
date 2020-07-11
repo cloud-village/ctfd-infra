a0dedd1c1ecf3129df755cf097005ce0c424545c
Max G <maxdotdotg@gmail.com>
2020-07-11 18:48:17 -0400

    add alb to main template

--- autoscaler/main.tf
+++ autoscaler/main.tf
@@ -1,4 +1,4 @@
-module "ctfd-launch-template" {
+module "ctfd_launch_template" {
   source = "./launch-template"
   iam_instance_profile_name = "" #TK
   image_id  = "" #TK
@@ -6,7 +6,7 @@ module "ctfd-launch-template" {
   vpc_security_group_ids = "" #TK
 }
 
-module "ctfd-autoscaling-group" {
+module "ctfd_autoscaling_group" {
   source = "./autoscaling_group"
   availability_zones  = "" #TK
   desired_capacity = "" #TK
@@ -14,3 +14,11 @@ module "ctfd-autoscaling-group" {
   launch_template_id = module.ctfd-launch-template.id
 }
 
+module "ctfd_alb" {
+  source = "./alb"
+  security_groups = "" #TK
+  subnets = "" #TK
+  alb_logs_bucket_name = "" #TK
+  vpc_id = "" #TK
+  certificate_arn = "" #TK
+}
