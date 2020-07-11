bc55fcf15e02fb6e7afc32158f3620b2df6f81fd
Max G <maxdotdotg@gmail.com>
2020-07-10 22:57:47 -0400

    output password sensitive, terraform fmt

--- mysql_db/outputs.tf
+++ mysql_db/outputs.tf
@@ -5,3 +5,8 @@ output "db_cluster" {
 output "db_instances" {
   value = aws_rds_cluster_instance.cluster_instances[*]
 }
+
+output "pass" {
+  value     = random_string.random
+  sensitive = true
+}
