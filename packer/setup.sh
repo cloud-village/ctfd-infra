94f07bfde226af69be425dc4ae4e94265b203c51
Max G <maxdotdotg@gmail.com>
2020-07-12 00:45:51 -0400

    packer template, updated readme for todo list, fix pip issue in setup.sh

--- packer/setup.sh
+++ packer/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # update
-apt update -y 
+sudo apt-get update -y 
 
 # install apt deps
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
@@ -17,15 +17,15 @@ pip install gunicorn virtualenv
 
 #get ctfd
 wget -q https://github.com/CTFd/CTFd/archive/2.5.0.zip
-unzip 2.5.0.zip -d /opt
+sudo unzip 2.5.0.zip -d /opt
 
 # create ctfd user
 sudo useradd ctfd -d /opt/CTFd-2.5.0
 sudo chown -R ctfd.ctfd /opt/CTFd-2.5.0
 
 # install python deps
-sudo su ctfd -c "pip install virtualenv"
-sudo su ctfd -c "virtualenv /opt/CTFd-2.5.0"
+sudo su ctfd -c "pip install virtualenv --user"
+sudo su ctfd -c "/opt/CTFd-2.5.0/.local/bin/virtualenv /opt/CTFd-2.5.0"
 sudo su ctfd -c "/opt/CTFd-2.5.0/bin/pip install -r /opt/CTFd-2.5.0/requirements.txt"
 
 # copy systemd unit
