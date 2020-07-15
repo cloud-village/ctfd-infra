028dc2559ddb33fb4b9c20b5d0467ba7c9d4430e
Max G <maxdotdotg@gmail.com>
2020-07-15 18:28:17 -0400

    terraform - remove unused variable from IAM module

--- terraform/iam/variables.tf
+++ terraform/iam/variables.tf
@@ -3,7 +3,3 @@ variable "uploads_bucket_name" {
   description = "name of the s3 bucket uploads will be saved to"
 }
 
-variable "region" {
-  type = string
-  description = "aws region the resources will live in"
-}
