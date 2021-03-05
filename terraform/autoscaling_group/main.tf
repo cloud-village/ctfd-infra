319379239ce0904a5da1b5f5ad5d7b2d222e3dd0
Max G <maxdotdotg@gmail.com>
2021-03-05 17:40:41 -0500

    Revert "terraform - remove autoscaling policy"
    This reverts commit a980e83dbfe12225edc486f0edc4dd4d5b17db39.

--- terraform/autoscaling_group/main.tf
+++ terraform/autoscaling_group/main.tf
@@ -18,3 +18,55 @@ resource "aws_autoscaling_group" "ctfd_asg" {
   depends_on = [var.asg_depends_on]
 }
 
+resource "aws_autoscaling_policy" "scale_up" {
+  name                   = "ctfd-scale-up"
+  scaling_adjustment     = 2
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.ctfd_asg.name
+}
+
+resource "aws_cloudwatch_metric_alarm" "ctfd_high_cpu" {
+  alarm_name          = "ctfd_high_cpu"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = var.scale_up_threshold
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.ctfd_asg.name
+  }
+
+  alarm_description = "This metric monitors ec2 cpu utilization"
+  alarm_actions     = [aws_autoscaling_policy.scale_up.arn]
+}
+
+resource "aws_autoscaling_policy" "scale_down" {
+  name                   = "ctfd-scale-down"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.ctfd_asg.name
+}
+
+resource "aws_cloudwatch_metric_alarm" "ctfd_low_cpu" {
+  alarm_name          = "ctfd_low_cpu"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "300"
+  statistic           = "Average"
+  threshold           = var.scale_down_threshold
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.ctfd_asg.name
+  }
+
+  alarm_description = "This metric monitors ec2 cpu utilization"
+  alarm_actions     = [aws_autoscaling_policy.scale_down.arn]
+}
+
