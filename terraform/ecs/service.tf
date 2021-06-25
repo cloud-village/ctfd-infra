8288d4ef6476cd6ba057dd8391e111d8898f0a12
Max G <maxdotdotg@gmail.com>
2021-06-25 11:16:02 -0400

    terraform - add ecs service

---
+++ terraform/ecs/service.tf
@@ -0,0 +1,18 @@
+resource "aws_ecs_service" "ctfd" {
+  name            = "ctfd-service"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.task.arn
+  desired_count   = 3
+  launch_type     = "FARGATE"
+
+  iam_role   = var.iam_role_arn
+  depends_on = [var.iam_role_policy]
+
+  load_balancer {
+    target_group_arn = var.target_group_arn
+    container_name   = "ctfd"
+    container_port   = 8000
+  }
+
+}
+
