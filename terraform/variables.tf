de9cae10583c3490c4f7836e8eb050c5a807dce7
Max G <maxdotdotg@gmail.com>
2021-06-26 11:04:22 -0400

    terraform - remove unused variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,18 +1,3 @@
-variable "packer_image_id" {
-  type        = string
-  description = "ami build by packer"
-}
-
-variable "template_az" {
-  type        = string
-  description = "availability zone used by the launch template"
-}
-
-variable "key_name" {
-  type        = string
-  description = "ec2 keypair"
-}
-
 variable "vpc_id" {
   type        = string
   description = "vpc_id that all the things will live in"
@@ -24,17 +9,6 @@ variable "availability_zones" {
   description = "availability zones where things will live"
 }
 
-variable "max_size" {
-  type        = number
-  description = "what is the maximum size of the autoscaling group?"
-}
-
-variable "desired_capacity" {
-  type        = number
-  description = "how many nodes should there be to start? at least the same number of availability zones"
-  default     = 2
-}
-
 variable "alb_subnets" {
   type        = list(string)
   description = "subnet ids the ALB should live in"
@@ -43,16 +17,13 @@ variable "alb_subnets" {
 variable "certificate_arn" {
   type        = string
   description = "ARN for the SSL certificate used by the ALB"
-}
-
-variable "uploads_bucket_name" {
-  description = "name of the bucket for uploads"
-  type        = string
+  default = ""
 }
 
 variable "route53_zone_id" {
   type        = string
   description = "hosted zone id"
+  default = ""
 }
 
 variable "redis_node_type" {
@@ -86,7 +57,7 @@ variable "ctfd_version" {
 variable "workers" {
   type        = string
   description = "number of gunicorn workers"
-
+  default = 3
 }
 
 variable "mailfrom_addr" {
@@ -114,5 +85,4 @@ variable "logs_region" {
   type        = string
   description = "region to store logs"
 }
-
 
