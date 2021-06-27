216a5456780df33903794fe0b2d69da4eef0aa63
Max G <maxdotdotg@gmail.com>
2021-06-27 10:52:46 -0400

    terraform - fmt

--- terraform/alb/main.tf
+++ terraform/alb/main.tf
@@ -80,7 +80,7 @@ resource "aws_lb_listener" "http_only" {
   protocol          = "HTTP"
 
   default_action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = aws_lb_target_group.ctfd_target_group.arn
   }
 }
