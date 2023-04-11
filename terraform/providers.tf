421ef7f274b7e5c9cd572c1e31339b8841e6f55e
Max G <maxdotdotg@gmail.com>
2023-04-11 10:43:08 -0400

    terraform - update provider versions

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.47.0"
+      version = "~> 4.62.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.1.0"
+      version = "~> 3.4.0"
     }
     http = {
       source  = "hashicorp/http"
-      version = "2.1.0"
+      version = "~> 3.2.0"
     }
   }
 }
