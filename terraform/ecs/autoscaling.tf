e0cdddcb3446c503e303e568f42acedd1e58ce81
Max G <maxdotdotg@gmail.com>
2025-03-31 14:31:05 -0400

    y (#38)

--- terraform/ecs/autoscaling.tf
+++ terraform/ecs/autoscaling.tf
@@ -1,13 +1,23 @@
-# using CPU-driven scaling module from TF registry
-# https://registry.terraform.io/modules/cn-terraform/ecs-service-autoscaling/aws/latest
-
-module "ecs-service-autoscaling" {
-  source                    = "cn-terraform/ecs-service-autoscaling/aws"
-  version                   = "1.0.3"
-  ecs_cluster_name          = "${local.name}-cluster"
-  ecs_service_name          = aws_ecs_service.ctfd.name
-  name_prefix               = "ctfd"
-  scale_target_min_capacity = var.desired_count
-  max_cpu_threshold         = var.max_cpu_threshold
+resource "aws_appautoscaling_target" "asg_target" {
+  max_capacity       = 5
+  min_capacity       = var.desired_count
+  resource_id        = "service/${aws_ecs_cluster.cluster.name}/${aws_ecs_service.ctfd.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
 }
 
+resource "aws_appautoscaling_policy" "cpu" {
+  name               = "${local.name}-cpu-poilcy"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.asg_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.asg_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.asg_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    scale_in_cooldown = 60
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+    target_value = var.max_cpu_threshold
+  }
+}
