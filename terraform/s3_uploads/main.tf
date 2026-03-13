a7027afd1e0f40c4e4e8cbc88232cde23a679c57
Max G <maxdotdotg@gmail.com>
2026-03-13 19:26:42 -0400

    s3 - fmt

--- terraform/s3_uploads/main.tf
+++ terraform/s3_uploads/main.tf
@@ -1,6 +1,7 @@
 resource "aws_s3_bucket" "uploads_bucket" {
-  bucket = "${local.name}-${random_string.bucket_seed.result}"
-  acl    = "private"
+  force_destroy = true
+  bucket        = "${local.name}-${random_string.bucket_seed.result}"
+  acl           = "private"
 
   tags = {
     Name = local.name
